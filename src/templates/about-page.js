db7fcc3b07cd5ee245fd25a7e27354e8ffa70dd4
joshtom <olajidejoshua4real@gmail.com>
2020-06-02 19:00:01 +0100

    Update about-page component

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -18,30 +18,10 @@ const AboutPage = () => (
       <section className="vision">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-12 col-sm-12">
+            <div className="col-lg-8 col-sm-12">
               <h1 className=" h1 text-center">Our Vision</h1>
               <p className="p">
-                Sed velit lectus, porttitor eu convallis sit amet, semper eget
-                mauris. Integer in pulvinar mauris. Donec facilisis placerat
-                magna sed cursus. Mauris vel tristique arcu. Duis congue orci id
-                libero dictum sollicitudin. Curabitur dapibus arcu leo,
-                condimentum tempus augue condimentum sed. Aliquam sed auctor ex.
-                Nunc quis neque non eros dictum scelerisque ut ac urna. Etiam
-                vel felis molestie, malesuada neque tempus, bibendum mauris.
-                Nullam sit amet rhoncus nisl. Quisque non vehicula dui.
-                Vestibulum neque tortor, placerat sit amet condimentum euismod,
-                suscipit lobortis diam. Sed varius elit non erat dignissim
-                vulputate.ed velit lectus, porttitor eu convallis sit amet,
-                semper eget mauris. Integer in pulvinar mauris. Donec facilisis
-                placerat magna sed cursus. Mauris vel tristique arcu. Duis
-                congue orci id libero dictum sollicitudin. Curabitur dapibus
-                arcu leo, condimentum tempus augue condimentum sed. Aliquam sed
-                auctor ex. Nunc quis neque non eros dictum scelerisque ut ac
-                urna. Etiam vel felis molestie, malesuada neque tempus, bibendum
-                mauris. Nullam sit amet rhoncus nisl. Quisque non vehicula dui.
-                Vestibulum neque tortor, placerat sit amet condimentum euismod,
-                suscipit lobortis diam. Sed varius elit non erat dignissim
-                vulputate.
+               We are a platform centered around Illuminating Hearts, Reshaping earth through the word of God. We believe every lives can be touched and transformed by being exposed to the right content and making it free of charge to any individual irrespective of race, tribe, or religion.
               </p>
             </div>
           </div>
@@ -50,7 +30,7 @@ const AboutPage = () => (
       <section className="mission">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-lg-12 col-sm-12">
+            <div className="col-lg-8 col-sm-12">
               <h1 className="h1 text-center">Our Mission</h1>
               <p className="p">
               We are not denominational and never want to be. As we move faster to wiping away the cosmos, we intensify the need to break denominational barriers and bring as many million souls to the reality of heaven and hell thereby defeating the struggling lord of this world.
